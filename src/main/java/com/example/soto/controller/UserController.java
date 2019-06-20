247669d32b67314b5819ec3baed0d5f1e1a09a0a
Faon <1425214630@qq.com>
2019-06-20 15:04:33 +0800

    整合freemarker模板并启用热重载

--- src/main/java/com/example/soto/controller/UserController.java
+++ src/main/java/com/example/soto/controller/UserController.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class UserController {
     @Autowired
@@ -16,5 +21,25 @@ public class UserController {
     @ResponseBody
     public User  selectById(){
        return userMapper.selectById();
+    }
+
+
+    @RequestMapping("/test")
+    public  String  test(Map<String,Object> map){
+        map.put("name", "Joe");
+        map.put("sex", 1);    //sex:性别，1：男；0：女；
+        map.put("test","test");
+        // 模拟数据
+        List<Map<String, Object>> friends = new ArrayList<Map<String, Object>>();
+        Map<String, Object> friend = new HashMap<String, Object>();
+        friend.put("name", "xbq");
+        friend.put("age", 22);
+        friends.add(friend);
+        friend = new HashMap<String, Object>();
+        friend.put("name", "July");
+        friend.put("age", 18);
+        friends.add(friend);
+        map.put("friends", friends);
+        return "freemarker";
     }
 }
